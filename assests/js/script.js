005fc11eba09e84226e0fb04c4979b46ce478960
jaycee808 <jamie.clark@live.co.uk>
2023-01-22 16:16:48 +0000

    added variables with data value for time-blocks

--- assests/js/script.js
+++ assests/js/script.js
@@ -9,3 +9,28 @@ $('#currentDay').append(currentDay);
 $('#currentTime').append(currentTime);
 
 // variables for time blocks
+var hour1 = document.querySelector('#hour1');
+var hour2 = document.querySelector('#hour2');
+var hour3 = document.querySelector('#hour3');
+var hour4 = document.querySelector('#hour4');
+var hour5 = document.querySelector('#hour5');
+var hour6 = document.querySelector('#hour6');
+var hour7 = document.querySelector('#hour7');
+var hour8 = document.querySelector('#hour8');
+var hour9 = document.querySelector('#hour9');
+
+console.log(hour1.dataset.hour);
+console.log(hour2.dataset.hour);
+console.log(hour3.dataset.hour);
+console.log(hour4.dataset.hour);
+console.log(hour5.dataset.hour);
+console.log(hour6.dataset.hour);
+console.log(hour7.dataset.hour);
+console.log(hour8.dataset.hour);
+console.log(hour9.dataset.hour);
+
+
+
+
+
+
