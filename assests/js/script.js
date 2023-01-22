5038c39c0dd3a05892ce4db338a1532eb505b4ec
jaycee808 <jamie.clark@live.co.uk>
2023-01-22 17:37:44 +0000

    added active id 2 input 2 change color when active

--- assests/js/script.js
+++ assests/js/script.js
@@ -35,28 +35,37 @@ var compareHour = moment().format('HH');
 // if statements to compare current hour with data-hour value
 if (hour1 && compareHour == 9) {
     console.log("it's 9am");
+    $('#active9').addClass('present');
 }
 if (hour2 && compareHour == 10) {
     console.log("it's 10am");
+    $('#active10').addClass('present');
 }
 if (hour3 && compareHour == 11) {
     console.log("it's 11am");
+    $('#active11').addClass('present');
 }
 if (hour4 && compareHour == 12) {
     console.log("it's 12pm");
+    $('#active12').addClass('present');
 }
 if (hour5 && compareHour == 13) {
     console.log("it's 1pm");
+    $('#active13').addClass('present');
 }
 if (hour6 && compareHour == 14) {
     console.log("it's 2pm");
+    $('#active14').addClass('present');
 }
 if (hour7 && compareHour == 15) {
     console.log("it's 3pm");
+    $('#active15').addClass('present');
 }
 if (hour8 && compareHour == 16) {
     console.log("it's 4pm");
+    $('#active16').addClass('present');
 }
 if (hour9 && compareHour == 17) {
     console.log("it's 5pm");
+    $('#active17').addClass('present');
 }
